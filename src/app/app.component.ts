199919aacffb209d419c5256035a75b7cf704203
Split <hyperschnell11@outlook.sk>
2023-08-19 14:45:38 -0400

    installed font awesome and made introduction section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NbIconLibraries} from "@nebular/theme";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PF2023';
+
+  constructor(private iconLibraries: NbIconLibraries) {
+
+    this.iconLibraries.registerFontPack('font-awesome', {
+      packClass: 'fa',
+      iconClassPrefix: 'fa',
+      icons: [
+        {name: 'sack-dollar', iconClass: 'fa-sack-dollar'},
+        {name: 'table-list', iconClass: 'fa-table-list'},
+        {name: 'coins', iconClass: 'fa-coins'}
+      ],
+    });
+
+    this.iconLibraries.registerFontPack('font-awesome-brands', {
+      packClass: 'fab',
+      iconClassPrefix: 'fa',
+      icons: [
+        {name: 'discord', iconClass: 'fa-discord'},
+      ],
+    });
+
+    // Register Nebular's default icon pack (required)
+    this.iconLibraries.setDefaultPack('eva');
+  }
 }
